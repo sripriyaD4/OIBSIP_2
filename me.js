3894198ed2cf83db27c31054ffc5925fc11f6392
sripriyaD4 <124561816+sripriyaD4@users.noreply.github.com>
2023-09-15 15:26:51 +0530

    Add files via upload

---
+++ me.js
@@ -0,0 +1,28 @@
+let MenuBtn= document.getElementById('MenuBtn')
+
+MenuBtn.addEventListener('click',function(e) {
+
+    document.querySelector('body').classList.toggle('mobile-nav-active');
+    this.classList.toggle('fa-xmark')
+})
+
+let typed = new Typed('.auto-input ',{
+    strings:['student'],
+    typeSpeed: 100,
+    backSpeed:100,
+    backDelay: 2000,
+    loop: true,
+})
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link")
+    document.getElementById(tabname).classList.add("active-tab");
+}
